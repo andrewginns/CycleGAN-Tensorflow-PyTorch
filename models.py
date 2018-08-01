5658c0f07621fb37eb484eb22e516204b93c4271
Andrew Ginns <ginns.aw@gmail.com>
2018-08-01 17:06:03 +0100

    Added depthwise convolution from mobilenet
    Theoretical faster convolutions from the https://arxiv.org/pdf/1704.04861.pdf
    
    Implemented with TF-Slim's seperable_conv
    
    Slower than standard convolution https://github.com/tensorflow/tensorflow/issues/12132

--- models.py
+++ models.py
@@ -7,8 +7,12 @@ from functools import partial
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
-
+# Normal convolution
 conv = partial(slim.conv2d, activation_fn=None)
+
+# Mobilenet depthwise convolution
+conv2 = partial(slim.separable_conv2d, activation_fn=None)
+
 deconv = partial(slim.conv2d_transpose, activation_fn=None)
 relu = tf.nn.relu
 lrelu = partial(tf.nn.leaky_relu, alpha=0.2)
@@ -17,39 +21,82 @@ batch_norm = partial(slim.batch_norm, scale=True, decay=0.9, epsilon=1e-5, updat
 
 def discriminator(img, scope, dim=64, train=True):
     bn = partial(batch_norm, is_training=train)
+    
+#    Normal method
     conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+    
+#    Depthwise seperable convolution
+    depth_conv = partial(conv2, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
 
     with tf.variable_scope(scope + '_discriminator', reuse=tf.AUTO_REUSE):
         net = lrelu(conv(img, dim, 4, 2))
-        net = conv_bn_lrelu(net, dim * 2, 4, 2)
-        net = conv_bn_lrelu(net, dim * 4, 4, 2)
-        net = conv_bn_lrelu(net, dim * 8, 4, 1)
+        net = depth_conv(net, None, dim * 2, 4, 2)
+        net = depth_conv(net, None, dim * 4, 4, 2)
+        net = depth_conv(net, None, dim * 8, 4, 1)
         net = conv(net, 1, 4, 1)
 
         return net
 
+#def discriminator(img, scope, dim=64, train=True):
+#    bn = partial(batch_norm, is_training=train)
+#    conv_bn_lrelu = partial(conv, normalizer_fn=bn, activation_fn=lrelu, biases_initializer=None)
+#
+#    with tf.variable_scope(scope + '_discriminator', reuse=tf.AUTO_REUSE):
+#        net = lrelu(conv(img, dim, 4, 2))
+#        net = conv_bn_lrelu(net, dim * 2, 4, 2)
+#        net = conv_bn_lrelu(net, dim * 4, 4, 2)
+#        net = conv_bn_lrelu(net, dim * 8, 4, 1)
+#        net = conv(net, 1, 4, 1)
+#
+#        return net
 
 def generator(img, scope, dim=64, train=True):
     bn = partial(batch_norm, is_training=train)
     conv_bn_relu = partial(conv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+    # conv_bn_relu = partial(conv, depth_multiplier=1, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
     deconv_bn_relu = partial(deconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
-
+    
     def _residule_block(x, dim):
         y = conv_bn_relu(x, dim, 3, 1)
         y = bn(conv(y, dim, 3, 1))
         return y + x
-
+    
     with tf.variable_scope(scope + '_generator', reuse=tf.AUTO_REUSE):
         net = conv_bn_relu(img, dim, 7, 1)
         net = conv_bn_relu(net, dim * 2, 3, 2)
         net = conv_bn_relu(net, dim * 4, 3, 2)
-
+        
         for i in range(9):
             net = _residule_block(net, dim * 4)
-
+    
         net = deconv_bn_relu(net, dim * 2, 3, 2)
         net = deconv_bn_relu(net, dim, 3, 2)
         net = conv(net, 3, 7, 1)
         net = tf.nn.tanh(net, name="output_image")
+        
+    return net
 
-        return net
+#def generator(img, scope, dim=64, train=True):
+#    bn = partial(batch_norm, is_training=train)
+#    conv_bn_relu = partial(conv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+#    deconv_bn_relu = partial(deconv, normalizer_fn=bn, activation_fn=relu, biases_initializer=None)
+#
+#    def _residule_block(x, dim):
+#        y = conv_bn_relu(x, dim, 3, 1)
+#        y = bn(conv(y, dim, 3, 1))
+#        return y + x
+#
+#    with tf.variable_scope(scope + '_generator', reuse=tf.AUTO_REUSE):
+#        net = conv_bn_relu(img, dim, 7, 1)
+#        net = conv_bn_relu(net, dim * 2, 3, 2)
+#        net = conv_bn_relu(net, dim * 4, 3, 2)
+#
+#        for i in range(9):
+#            net = _residule_block(net, dim * 4)
+#
+#        net = deconv_bn_relu(net, dim * 2, 3, 2)
+#        net = deconv_bn_relu(net, dim, 3, 2)
+#        net = conv(net, 3, 7, 1)
+#        net = tf.nn.tanh(net, name="output_image")
+#
+#        return net
