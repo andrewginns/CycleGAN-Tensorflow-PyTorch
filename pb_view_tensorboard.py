592b7f1d306e64396944eb729f5d7daab195e581
Andrew Ginns <ginns.aw@gmail.com>
2018-08-13 15:03:24 +0100

    Fix to viewing pb in tensorboard

--- pb_view_tensorboard.py
+++ pb_view_tensorboard.py
@@ -7,7 +7,7 @@ parser = argparse.ArgumentParser(description='')
 parser.add_argument('--graph', dest='graph_filename', default='graph.pb', help='which graph to use')
 args = parser.parse_args()
 
-graph_filename = args.model_filename
+graph_filename = args.graph_filename
 
 # Modified from https://gist.github.com/jubjamie/2eec49ca1e4f58c5310d72918d991ef6#file-pb_viewer-py
 
@@ -16,6 +16,6 @@ with tf.Session() as sess:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
         g_in = tf.import_graph_def(graph_def)
-LOGDIR='logs/' + graph_filename
+LOGDIR='/tmp/tensorboard'
 train_writer = tf.summary.FileWriter(LOGDIR)
 train_writer.add_graph(sess.graph)
